2dfd7a3ab006817ecdd0c86f67538310daa5bd75
yuval weber <yuval199985@gmail.com>
2025-01-15 13:30:08 +0200

    NET-203 - Added Icon component for icon on top bar

---
+++ frontend/src/components/icon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Icon = () => { return(
+    <div className="top-right-icon">
+        <img src="/images/icon.png" alt="Icon" className="icon-image-racoon"/>
+    </div>
+);};
+
+export default Icon;
