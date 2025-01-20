a16496167cfab5fb2304f4d882f87ecaa55e60c3
shayZingboim <sszingboim@gmail.com>
2025-01-20 11:55:38 +0200

    NET-204 - finish to create the video player

--- frontend/src/pages/watchPage.js
+++ frontend/src/pages/watchPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';	
+import React, {useEffect} from 'react';
+import { useNavigate, useLocation} from 'react-router-dom';	
 import WatchNavbar from '../components/watchNavbar.js'; // ה-Navbar החדש
 import WatchPlayer from '../components/watchPlayer'; // נגן הווידאו
 import '../components/watchPage.css'; // סגנונות עמוד הצפייה
@@ -7,6 +7,31 @@ import '../components/watchPage.css'; // סגנונות עמוד הצפייה
 const WatchPage = () => {
   const location = useLocation();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    // Set overflow to hidden when the component is loaded
+    document.documentElement.style.overflow = "hidden"; // For <html>
+    document.body.style.overflow = "hidden"; // For <body>
+	document.documentElement.style.height = "100%"; // For <html>
+	document.body.style.height = "100%"; // For <body>
+	document.documentElement.style.margin = "0"; // For <html>
+	document.body.style.margin = "0"; // For <body>
+	document.documentElement.style.padding = "0"; // For <html>
+	document.body.style.padding = "0"; // For <body>
+
+    return () => {
+      // Clean up: Reset overflow when leaving the page
+	  document.documentElement.style.overflow = ""; // For <html>
+	  document.body.style.overflow = ""; // For <body>
+	  document.documentElement.style.height = ""; // For <html>
+	  document.body.style.height = ""; // For <body>
+	  document.documentElement.style.margin = ""; // For <html>
+	  document.body.style.margin = ""; // For <body>
+	  document.documentElement.style.padding = ""; // For <html>
+	  document.body.style.padding = ""; // For <body>
+
+    };
+  }, []);
 
   // get the videoUrl from the location state
   const videoUrl = location.state?.trailer;
@@ -16,6 +41,8 @@ const WatchPage = () => {
 	navigate('/');
 	return null;
   }
+
+
 
   return (
     <div className="watch-page">
