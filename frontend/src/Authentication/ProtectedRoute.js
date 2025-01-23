21e71ed504df6d6bfd7692509ff94137bd801be0
yuval weber <yuval199985@gmail.com>
2025-01-23 08:36:49 +0200

    NET-246 - Fixed issue with authContext

--- frontend/src/Authentication/ProtectedRoute.js
+++ frontend/src/Authentication/ProtectedRoute.js
@@ -1,12 +1,28 @@
 import { useAuth } from "./AuthContext";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8080";
 
 export const ProtectedRoute = ({ Component }) => {
-  const { token } = useAuth();
+  const { token, userData, setUserData } = useAuth();
 
   // If there's no token, redirect to login page
   if (!token) {
     return <Navigate to="/login" />;
+  }
+
+  // if token still works and user data is not fetched, fetch user data
+  if(!userData){
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    const response = axios.get("/api/tokens");
+    response.then((res) => {
+      setUserData(res.data);
+    }).catch((error) => {
+      localStorage.removeItem("token");
+      return <Navigate to="/login" />;
+    });
+
   }
 
   // If authenticated, render the protected component
