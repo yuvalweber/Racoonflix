908e5cb959fb50691b4c7889bd3e57bae6089cf4
yairk55 <yairk55@gmail.com>
2025-01-28 20:58:01 +0200

    NET-264 - make profile button

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,12 +1,53 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { ThemeContext } from './themeContext';
 import Logout from './logout';
 import './navbar.css';
+import axios from 'axios';
+import { useAuth } from '../Authentication/AuthContext';
+
+
+// Set the base URL for the API
+axios.defaults.baseURL = 'http://localhost:8080';
 
 // Navbar component
 const Navbar = ({ isAdmin }) => {
+  const { token , userData } = useAuth();
   const { isDarkMode, toggleTheme } = useContext(ThemeContext); // Use ThemeContext
+  const [showModal, setShowModal] = useState(false);
+  const [userInfo, setUserInfo] = useState(null);
+  const [userId, setUserId] = useState('');
+  
+
+  // State for showing the modal
+  const fetchUserInfo = async () => {
+    try { 
+      const response = await axios.get(`/api/users/${userId}`);
+      setUserInfo(response.data);
+    } catch (error) {
+      console.error('Error fetching user info:', error);
+    }
+  };
+
+  // State for showing the modal
+  const handleProfileClick = () => {
+    setShowModal(true);
+    fetchUserInfo();
+  };
+
+  // Close the modal
+  const closeModal = () => setShowModal(false);
+
+   // UseEffect to check if the user is an admin and set the Authorization header
+   useEffect(() => {
+		if (token) {
+		  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+		}
+		if (userData) {
+      setUserId(userData.id);
+		}
+	  }, [token, userData]); // Set the Authorization header once on mount
+
 
   return (
     // Navbar with links to home, all movies, search, theme toggle, logout, and management
@@ -28,6 +69,53 @@ const Navbar = ({ isAdmin }) => {
               <Link className="nav-link" to="/search">Search</Link>
             </li>
           </ul>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <button className="btn btn-primary me-2" onClick={handleProfileClick}>
+                Profile
+              </button>
+            </li>
+          </ul>
+          {/* Modal */}
+          {showModal && (
+            <div className="custom-modal-overlay">
+              <div className="custom-modal">
+                <div className="custom-modal-header">
+                  <h5 className="custom-modal-title">User Profile</h5>
+                  <button className="custom-modal-close" onClick={closeModal}>&times;</button>
+                </div>
+                <div className="custom-modal-body">
+                  {userInfo ? (
+                    <div className="user-info">
+                      {/* Profile Picture */}
+                      {userInfo.profilePicture && (
+                        <div className="profile-picture-container">
+                          <img
+                            src={userInfo.profilePicture}
+                            alt={""}
+                            className="profile-picture"
+                          />
+                        </div>
+                      )}
+                      {/* User Info */}
+                      <p><strong>First Name:</strong> {userInfo.firstName}</p>
+                      <p><strong>Last Name:</strong> {userInfo.lastName}</p>
+                      <p><strong>Username:</strong> {userInfo.userName}</p>
+                      <p><strong>Email:</strong> {userInfo.email}</p>
+                      <p><strong>Admin:</strong> {userInfo.isAdmin ? 'Yes' : 'No'}</p>
+                    </div>
+                  ) : (
+                    <p>Loading...</p>
+                  )}
+                </div>
+                <div className="custom-modal-footer">
+                  <button className="custom-modal-button" onClick={closeModal}>
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
           <ul className="navbar-nav">
             <li className="nav-item">
                 {/* Button to toggle between light and dark mode */}
